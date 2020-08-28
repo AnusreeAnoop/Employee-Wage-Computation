a7d521251d5e2101a650e1ecc38827e9766887b9
Anusree <anoopanusree31@gmail.com>
2020-08-28 16:32:00 +0530

    Added part time and full time and wage

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -8,10 +8,21 @@ public class EmpWageBuilder
 		int empHour=8;
 		int empRatePerHour=20;
 		int empSalary=0;
+		int partTime=0;
+		int fullTime=1;
 
 		double empPresent = Math.floor(Math.random() * 10) % 2;
 		if ( empPresent == isPresent ) {
 			System.out.println("Employee is Present");
+			double emp = Math.floor(Math.random() * 10) % 2;
+			if ( emp == partTime ) {
+				System.out.println("Employee is part time Present");
+				empHour=4;
+			}
+			else {
+				System.out.println("Employee is full time Present");
+				empHour=8;
+			}
 			empSalary=empRatePerHour*empHour;
 		}
 		else {
