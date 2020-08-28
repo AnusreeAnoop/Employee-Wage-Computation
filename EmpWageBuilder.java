a9f2eeab887095367471926250ce7d16f40904f2
Anusree <anoopanusree31@gmail.com>
2020-08-28 18:31:45 +0530

    Solved using switch case

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,41 +1,34 @@
-class EmpWageBuilder
-{
-	static int empHour=0;
-	static int empRatePerHour=20;
-	static int empSalary=0;
-	static final int partTime=0;
-	static final int fullTime=1;
-
-	static double getRandom(){
-		return Math.floor(Math.random() * 10) % 3;
-	}
-
-	static int getEmployeeHours(int empPresent){
-
-		int employeeHour=0;
-		switch(empPresent)
-		{
-			case partTime:
-					System.out.println("Employee is part time Present");
-					employeeHour=4;
-					break;
-			case fullTime:
-					System.out.println("Employee is full time Present");
-					employeeHour=8;
-					break;
-			default:
-					System.out.println("Employee is Absent");
-		}
-		return employeeHour;
-	}
+class EmpWageBuilder {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Program");
 
-		double empPresent = getRandom();
-		empHour = getEmployeeHours((int)empPresent);
+		System.out.println(" Welcome To Employee Wage Computation Program ");
+
+		int empHour=0;
+		int empRatePerHour=20;
+		int empSalary=0;
+		final int partTime=0;
+		final int fullTime=1;
+		
+		double empPresent = Math.floor(Math.random() * 10) % 3;
+		switch ((int)empPresent) {
+
+			case partTime:
+					System.out.println("Employee is part time Present");
+					empHour=4;
+					break; 
+			case fullTime:
+					System.out.println("Employee is full time Present");
+					empHour=8;
+					break;
+			default:
+					empHour=0;
+					System.out.println("Employee is Absent");
+					break;
+
+		}
 
 		empSalary=empRatePerHour*empHour;
-		System.out.println("Salary is :" + empSalary);
+		System.out.println("Salary is :" +empSalary);
 	}
 }
